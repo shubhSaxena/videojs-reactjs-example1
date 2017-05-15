8707e0ca92f55cd294d6f55e4fc4560c3879a832
Shubham Saxena <shubhamsaxena04@gmail.com>
2017-05-15 17:10:36 +0530

    test changesjhjh

--- src/index.js
+++ src/index.js
@@ -11,11 +11,12 @@ var Component = React.createClass({
     className="video-js vjs-default-skin"
     controls
     preload="auto"
-    poster="//vjs.zencdn.net/v/oceans.png">
+    poster="//vjs.zencdn.net/v/oceans.png"
+    data-setup='{}'>
+
   <source src="http://bc-jsanford.s3.amazonaws.com/instant_play_demo/1402004163/index.m3u8" type="application/x-mpegURL"></source>
   <p class="vjs-no-js">
-    consider upgrading to a
-    hello this is a change
+    To view this video please enable JavaScript, and consider upgrading to a
     web browser that
     <a href="http://videojs.com/html5-video-support/" target="_blank">
       supports HTML5 video
